96da136afc0db188ac128073c73cee22fae95e46
Luna <lunatattersall@gmail.com>
2026-05-17 11:56:07 +0100

    Projects Grid

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,24 +1,37 @@
 import lunadesignes from "../assets/projects/lunadesignes.png";
-
+import Github from "../assets/home/github-mark-white.png";
 
 
 export default function Projects() {
   return (
     <>
-      <p className="pt-4 mb-4 mx-auto text-center">
-        Welcome to my Projects heres what im working on.
-      </p>
-      <div className="grid 3xl:grid-cols-4 2xl:grid-cols-3 gap-8 lg:grid-cols-2 grid-cols-1 mx-auto ">
-        <div className="flex gap-4 flex-col items-start">
-          <div className="">
-            <img src={lunadesignes}  className=" w-full  max-w-sm md:min-w-lg xl:min-w-1xl 2xl:min-w-2xl border p-4 rounded-lg object-cover "></img>
-          </div>
-          <div className="projectComment w-full max-w-sm md:min-w-lg xl:min-w-1xl 2xl:min-w-2xl break-words">
-            This is the website you are currently on! Luna designes was a project of mine what i have wanted to finish 
-            for a long time but it turns our i love just working on it
-          </div>
+    <p className="pt-4 mb-4 mx-auto text-center">
+      Welcome to my Projects heres what im working on.
+    </p>
+
+    <div className="grid 3xl:grid-cols-2 2xl:grid-cols-2 lg:grid-cols-2 gap-100 grid-cols-1 mx-auto ">
+      <div className="flex gap-4 flex-col items-start">
+        <div className="">
+          <img src={lunadesignes}  className=" w-full  max-w-sm md:min-w-lg xl:min-w-1xl 2xl:min-w-1xl border p-4 rounded-lg object-cover "></img>
+        </div>
+        <div className="projectComment w-full max-w-sm md:min-w-lg xl:min-w-1xl 2xl:min-w-md break-words">
+          This is the website you are currently on! Luna designes was a project of mine what i have wanted to finish 
+          for a long time but it turns our i love just working on it <br></br><a className="text-blue-600 flex justify-center" href="https://github.com/Lunadrite/PortfolioWebsiteLunadrite">Github Repo<img src={Github} className="w-[24px] bg-black rounded-4xl "></img></a>
         </div>
       </div>
-    </>
+
+      <div className="flex gap-4 flex-col items-end">
+        <div className="">
+          <img src={lunadesignes}  className=" w-full  max-w-sm md:min-w-lg xl:min-w-1xl 2xl:min-w-1xl border p-4 rounded-lg object-cover "></img>
+        </div>
+        <div className="projectComment w-full max-w-sm md:min-w-lg xl:min-w-1xl 2xl:min-w-md break-words">
+          This is the website you are currently on! Luna designes was a project of mine what i have wanted to finish 
+          for a long time but it turns our i love just working on it <br></br><a className="text-blue-600 flex justify-center" href="https://github.com/Lunadrite/PortfolioWebsiteLunadrite">Github Repo<img src={Github} className="w-[24px] bg-black rounded-4xl "></img></a>
+        </div>
+      </div>
+
+    </div>
+
+  </>
   );
 }
