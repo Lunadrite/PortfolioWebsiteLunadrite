db26e159173d73b59339b181e40ab1371d1e263a
Luna <lunatattersall@gmail.com>
2026-06-08 12:44:46 +0100

    Add CLI project entry and asset
    Add new CLI project image asset and include a corresponding project card in src/pages/Projects.tsx. Imports the new cliProject image and adds a Blog.Projects entry with a caption and link to the repository (https://github.com/Lunadrite/CLI-Terminal). Also adjusts import/formatting around the existing GLFW project entry.

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,6 +1,7 @@
 import lunadesignes from "../assets/projects/lunadesignes.png";
 import Securechat from "../assets/projects/SecureChat.png";
 import glfw from "../assets/projects/glfw.png"
+import cliProject from "../assets/projects/cliProject.png"
 import { Blog } from "../components/blog/Blog"
 
 export default function Projects() {
@@ -25,10 +26,16 @@ export default function Projects() {
        href="https://github.com/Lunadrite/SecureChat"
        ></Blog.Projects>
 
-             <Blog.Projects 
+      <Blog.Projects 
        src={glfw}
        caption="A beginner OpenGL project built in C++ using GLFW and GLAD, focused on learning the fundamentals of computer graphics, rendering pipelines, shaders, and window management. The project explores concepts such as buffers, vertex and fragment shaders, and rendering basic shapes like triangles while documenting the learning process throughout development."
        href="https://github.com/Lunadrite/OpenGL-Attempt"
+       ></Blog.Projects>
+
+      <Blog.Projects 
+       src={cliProject}
+       caption="This is my CLI project on a react platform! My aim was to recreate a linux terminal feel since i swapped to linux recently and had alot of fun learning and experimenting while making this project please enjoy!"
+       href="https://github.com/Lunadrite/CLI-Terminal"
        ></Blog.Projects>
 
     </div>
