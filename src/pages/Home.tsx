8c2acddda55afe551ac08064f051063affbe999f
Luna <lunatattersall@gmail.com>
2026-06-17 14:46:48 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       </div>
       <div className="flex items-center pt-4 flex-col md:flex-row justify-center ">
         <div className="border-2 px-24 py-4 flex rounded-3xl flex-col  border-[#f68989] ">
-          <img src={ImageofMe} className="w-100 rounded-xl"></img>
+          <img src={ImageofMe} className="w-70 rounded-xl"></img>
           <div className="rounded-md text-center font-bold text-2xl underline underline-offset-4 ">
             Luna
           </div>
