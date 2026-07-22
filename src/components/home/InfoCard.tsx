d1ad944cf6a00927c1b261794405f6fe1548002c
Luna <lunatattersall@gmail.com>
2026-07-22 08:41:10 +0100

    Infocard Template added, Fixed Row on Home

---
+++ src/components/home/InfoCard.tsx
@@ -0,0 +1,12 @@
+export default function InfoCard() {
+    return(
+        <>
+    <div className="flex pt-4 flex-col md:flex-row  ">
+        <div className="border-3 px-12 py-4 flex rounded-3xl flex-col  border-[#880808] ">
+
+            fart
+        </div>
+        </div>
+        </>
+    )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,12 @@
 import Card from "../components/home/Card";
+import InfoCard from "../components/home/InfoCard"
+
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col">
+    <main className="flex flex-row gap-5 h-auto">
       <Card></Card> {/*My info card :) */}
+      <InfoCard></InfoCard>
+   
     </main>
   );
 }
